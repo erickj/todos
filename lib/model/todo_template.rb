54b875d46054af5025f647d18821eff13d125dae
Erick Johnson <erick@ejjohnson.org>
2015-03-30 16:21:37 -0400

    * lib/model/todo_template.rb: removed unnecessary required attribute from 'belongs_to :owner'

--- lib/model/todo_template.rb
+++ lib/model/todo_template.rb
@@ -11,7 +11,7 @@ module Todo
       # See 'Customizing Associations':
       #   doc http://datamapper.org/docs/associations.html
       #   src https://github.com/datamapper/dm-core/blob/master/lib/dm-core/associations/relationship.rb
-      belongs_to :owner, 'Person', :child_key => :owner_id, :required => true
+      belongs_to :owner, 'Person', :child_key => :owner_id
     end
   end
 end

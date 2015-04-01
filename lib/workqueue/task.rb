6692508a9c5ce4c03f6dfefa0fbd8e7bb03f8353
Erick Johnson <erick@ejjohnson.org>
2015-04-01 15:45:00 -0400

    * lib/workqueue/task.rb: Added a builder DSL for tasks

--- lib/workqueue/task.rb
+++ lib/workqueue/task.rb
@@ -1,6 +1,19 @@
 require 'util/object_util'
 
 module WorkQueue
+
+  class TaskValidationError < StandardError
+    def initialize(field_errors = {})
+      @field_errors = field_errors
+      super 'Invalid values for fields: %s'%@field_errors.map { |pair| pair[0] }.join(',')
+    end
+
+    def error_for_field(name)
+      @field_errors[name]
+    end
+    alias :[] :error_for_field
+  end
+
   module TaskMixin
 
     def self.included(base)
@@ -30,12 +43,111 @@ module WorkQueue
       self == value
     end
 
+    attr_writer :field_values
+    attr_writer :errors
+
+    def error(name)
+      errors ||= {}
+      errors[name]
+    end
+
+    def method_missing(name, *args)
+      return field_value(name) if has_field?(name)
+      super(name, *args)
+    end
+
+    private
+    def field_value(name)
+      @field_values[name] || self.class.field_definitions[name].get_default
+    end
+
+    def has_field?(field)
+      self.class.has_field? field
+    end
+
     module ClassMethods
+
+      attr_reader :field_definitions
 
       def task_type(type=nil)
         @task_type ||= type
       end
+
+      def field(name)
+        @field_definitions ||= {}
+        raise ArgumentError, 'field %s already defined'%(name) if has_field? name
+
+        @field_definitions[name] = FieldDefinition.new
+      end
+
+      def has_field?(name)
+        @field_definitions.has_key? name
+      end
+
+      def build(field_values)
+        task = self.allocate
+
+        validation_errors = {}
+        @field_definitions.each do |name, defn|
+          is_valid, field_validation_errors = @field_definitions[name].check_is_valid(name, field_values[name])
+          unless is_valid
+            validation_errors[name] = field_validation_errors
+          end
+        end
+
+        unless validation_errors.empty?
+          raise TaskValidationError, validation_errors
+        end
+
+        task.field_values = field_values
+        task
+      end
     end
+
+    class FieldDefinition
+
+      def default(v)
+        @default = v
+        self
+      end
+
+      def required
+        validate { |v| raise 'missing required value' if v.nil? }
+        self
+      end
+
+      def type(klass)
+        validate { |v| raise 'expected type %s'%klass unless v.is_a? klass }
+        self
+      end
+
+      def validate(validator_proc=nil, &block)
+        @validators ||= []
+        @validators << validator_proc unless validator_proc.nil?
+        @validators << block if block_given?
+        self
+      end
+
+      def get_default
+        @default
+      end
+
+      def check_is_valid(name, value)
+        return [true, []] if @validators.nil? || @validators.empty?
+
+        validation_errors = []
+        @validators.each do |validator|
+          begin
+            validator.call(value)
+          rescue StandardError
+            validation_errors << $!.message
+          end
+        end
+
+        return [validation_errors.empty?, validation_errors]
+      end
+    end
+
   end
 
   class Task
--- spec/workqueue/task_mixin_spec.rb
+++ spec/workqueue/task_mixin_spec.rb
@@ -21,4 +21,63 @@ RSpec.describe WQ::TaskMixin, :wq do
     foo_task = WQ::Task.new(:foo)
     expect(foo_task =~ foo_task).to be
   end
+
+  context 'fields and build' do
+
+    let(:field_values) do
+      {
+        :errand_list => [:bank, :shopping],
+        :max_money => 100,
+        :done_by => Time.now + 3600
+      }
+    end
+
+    class DoErrandsTask
+      include WQ::TaskMixin
+
+      field(:errand_list).default []
+      field(:max_money).required.type Integer
+      field(:done_by).type(Time).validate do |time|
+        raise 'I don\'t have a time machine' unless time > Time.now
+      end
+    end
+
+    subject { DoErrandsTask.build field_values }
+
+    it 'builds a task from a hash of fields' do
+      expect(subject.errand_list).to eql [:bank, :shopping]
+      expect(subject.max_money).to be 100
+      expect(subject.done_by).to eql field_values[:done_by]
+    end
+
+    it 'uses default values for missing fields' do
+      field_values.delete :errand_list
+      subject = DoErrandsTask.build field_values
+      expect(subject.errand_list).to eql []
+    end
+
+    it 'validates required fields' do
+      field_values.delete :max_money
+      expect { DoErrandsTask.build field_values }.to raise_error do |error|
+        expect(error).to be_a WQ::TaskValidationError
+        expect(error[:max_money].first).to be =~ /^missing required value/
+      end
+    end
+
+    it 'validates expected types' do
+      field_values[:max_money] = 10.1
+      expect { DoErrandsTask.build field_values }.to raise_error do |error|
+        expect(error).to be_a WQ::TaskValidationError
+        expect(error[:max_money].first).to be =~ /^expected type/
+      end
+    end
+
+    it 'validates custom validations' do
+      field_values[:done_by] = Time.now - 3600
+      expect { DoErrandsTask.build field_values }.to raise_error do |error|
+        expect(error).to be_a WQ::TaskValidationError
+        expect(error[:done_by].first).to be == 'I don\'t have a time machine'
+      end
+    end
+  end
 end

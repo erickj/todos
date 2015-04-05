ba5cfc69aa339ef4124c8434ab8a3a102eeda231
Erick Johnson <erick@ejjohnson.org>
2015-04-04 23:47:54 -0400

    * lib/todo/mail/todo_mailer.rb: basic mail support implemented

--- lib/todo/mail/todo_mailer.rb
+++ lib/todo/mail/todo_mailer.rb
@@ -1,5 +1,8 @@
+require 'mandrill'
 require 'workqueue'
 require 'logging'
+require 'todo/command'
+require 'todo/model'
 
 module Todo
   module Mail
@@ -7,6 +10,10 @@ module Todo
       include WQ::RedisConsumer
       include WQ::Subscriber
       include Logging
+
+      def initialize
+        raise 'environment var MANDRILL_APIKEY not set for mandrill gem' unless ENV['MANDRILL_APIKEY']
+      end
 
       def redis=(redis)
         @redis = redis
@@ -22,7 +29,36 @@ module Todo
 
       def handle_pubsub_message(message)
         task_result = WQ::TaskSerializer.instance.deserialize(message)
-        log.info 'sending mail for result: %s' % task_result.original_task_uuid
+
+        case task_result.original_task_type
+        when Command::TaskType::CREATE_TODO
+          handle_create_todo_result(task_result)
+        end
+      end
+
+      def handle_create_todo_result(task_result)
+        log.info 'sending mail for created todo result: %s' % task_result.original_task_uuid
+        todo_tpl = Model::TodoTemplate.by_uuid! task_result.result_key
+        log.debug { todo_tpl }
+        send_mail todo_tpl
+      end
+
+      def send_mail(todo_tpl)
+        m = Mandrill::API.new
+        message = {
+          :subject=> "Re: %s" % todo_tpl.title,
+          :from_name=> "domail",
+          :text=>"Saved your todo:\n\n%s" % todo_tpl.description,
+          :to=>[
+            {
+              :email=> ENV['OVERRIDE_EMAIL_RECIPIENT'],
+              :name=> "Recipient1"
+            }
+          ],
+#          :html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",
+          :from_email=>ENV['FROM_EMAIL']
+        }
+        m.messages.send message
       end
     end
   end

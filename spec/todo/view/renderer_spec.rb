04125e39e8c32773fdf11b1f81cd6e08d7bdbbe4
Erick Johnson <erick@ejjohnson.org>
2015-04-15 09:01:24 -0400

    * spec/todo/view/renderer_spec.rb: specs for Todo::View::Renderer

--- spec/todo/view/renderer_spec.rb
+++ spec/todo/view/renderer_spec.rb
@@ -15,20 +15,32 @@ RSpec.describe Todo::View::Renderer, :view do
     }
   end
 
-  let(:render_block) { lambda { |tpl_name, mode, is_partial| template_data[tpl_name][mode] }}
+  subject do
+    Class.new do
+      include Todo::View::Renderer
+      view_layout :layout
+    end.new
+  end
 
-  context 'given a layout name' do
-
-    subject do
-      Class.new do
-        include Todo::View::Renderer
-        view_layout :layout
-      end.new
-    end
+  context 'given a render block' do
+    let(:render_block) { lambda { |tpl_name, mode, is_partial| template_data[tpl_name][mode] }}
 
     it 'renders the partial inside the layout' do
       rendered_str = subject.render :partial, :mode_foo, locals, &render_block
       expect(rendered_str).to be == 'Renderer Layout: Renderer Partial, local-x'
+    end
+  end
+
+  context 'given layout files' do
+
+    let(:layouts) {{ :layout => template_data[:layout] } }
+    let(:partials) {{ :partial => template_data[:partial] }}
+
+    it 'renders the partial inside the layout' do
+      run_with_template_files layouts, partials do
+        rendered_str = subject.render :partial, :mode_foo, locals
+        expect(rendered_str).to be == 'Renderer Layout: Renderer Partial, local-x'
+      end
     end
   end
 end
